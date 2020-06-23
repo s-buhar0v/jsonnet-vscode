6089bc6092b1e398a567676f771fe65c3854e8da
Sh4d0w <sh4d0w@Pathfinder.local>
2020-06-23 19:08:00 +0300

    to helloworld

--- src/extension.ts
+++ src/extension.ts
@@ -15,7 +15,7 @@ export function activate(context: vscode.ExtensionContext) {
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand(
-    "jsonnet-vscode.helloWorld1",
+    "jsonnet-vscode.helloWorld",
     () => {
       // The code you place here will be executed every time your command is executed
 
